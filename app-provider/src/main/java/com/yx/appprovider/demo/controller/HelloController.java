5e37f9d728c51c53e03163393c0681b21f68fee2
yx657990706 <657990706@qq.com>
2019-07-16 23:16:25 +0800

    init
    provider

---
+++ app-provider/src/main/java/com/yx/appprovider/demo/controller/HelloController.java
@@ -0,0 +1,79 @@
+package com.yx.appprovider.demo.controller;
+
+import com.yx.commonsmodel.myModel.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.List;
+import java.util.logging.Logger;
+
+
+/**
+ * @author jesse
+ * @version v1.0
+ * @project MicroService
+ * @Description
+ * @encoding UTF-8
+ * @date 2018/9/11
+ * @time 下午2:38
+ * @修改记录 <pre>
+ * 版本       修改人         修改时间         修改内容描述
+ * --------------------------------------------------
+ * <p>
+ * --------------------------------------------------
+ * </pre>
+ */
+@RestController
+public class HelloController {
+    private final Logger logger = Logger.getLogger("HelloController");
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String index() throws Exception {
+//        Thread.sleep(7*1000);
+        List<ServiceInstance> instances = client.getInstances("app-provider");
+        for (int i = 0; i < instances.size(); i++) {
+            logger.info("/hello,host:" + instances.get(i).getHost() + ",service_id:" + instances.get(i).getServiceId());
+        }
+        return "Hello World";
+    }
+
+    @RequestMapping(value = "/getbook1", method = RequestMethod.GET)
+    public Book book1() {
+        return new Book("三国演义", 90, "罗贯中", "花城出版社");
+    }
+
+    @RequestMapping(value = "/getbook2", method = RequestMethod.POST)
+    public Book book2(@RequestBody Book book) {
+        System.out.println(book.getName());
+        book.setPrice(33);
+        book.setAuthor("曹雪芹");
+        book.setPublisher("人民文学出版社");
+        return book;
+    }
+
+    @RequestMapping(value = "/hello1", method = RequestMethod.GET)
+    public String hello1(@RequestParam String name) {
+        return "hello " + name + "!";
+    }
+
+    @RequestMapping(value = "/hello2", method = RequestMethod.GET)
+    public Book hello2(@RequestHeader String name, @RequestHeader String author, @RequestHeader Integer price) throws UnsupportedEncodingException {
+        Book book = new Book();
+        book.setName(URLDecoder.decode(name,"UTF-8"));
+        book.setAuthor(URLDecoder.decode(author,"UTF-8"));
+        book.setPrice(price);
+        System.out.println(book);
+        return book;
+    }
+
+    @RequestMapping(value = "/hello3", method = RequestMethod.POST)
+    public String hello3(@RequestBody Book book) {
+        return "书名为：" + book.getName() + ";作者为：" + book.getAuthor();
+    }
+}
