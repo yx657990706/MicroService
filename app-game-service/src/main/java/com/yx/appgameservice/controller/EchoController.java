5e3f414353fa470fe25ea2b929ba06f2c6974dd0
yinxiong <yinxiong>
2019-10-24 22:34:46 +0800

    test

--- app-game-service/src/main/java/com/yx/appgameservice/controller/EchoController.java
+++ app-game-service/src/main/java/com/yx/appgameservice/controller/EchoController.java
@@ -1,6 +1,7 @@
 package com.yx.appgameservice.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,10 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date 2019/10/24 15:02
  **/
 @RestController
+@RefreshScope
 public class EchoController {
 
 
-    @Value("${uuu}")
+    @Value("${myName}")
     private String str;
 
     @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
