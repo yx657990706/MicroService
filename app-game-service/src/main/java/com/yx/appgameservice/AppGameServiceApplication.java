53d5aab8f307af74cfbbb001e12a7e33b4d37b55
yinxiong <yinxiong>
2019-10-24 23:20:52 +0800

    test

--- app-game-service/src/main/java/com/yx/appgameservice/AppGameServiceApplication.java
+++ app-game-service/src/main/java/com/yx/appgameservice/AppGameServiceApplication.java
@@ -1,13 +1,11 @@
 package com.yx.appgameservice;
 
-import com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@NacosConfigurationProperties(dataId = "app-game-service-dev.properties")
 public class AppGameServiceApplication {
 
     public static void main(String[] args) {
--- app-game-service/src/main/java/com/yx/appgameservice/controller/EchoController.java
+++ app-game-service/src/main/java/com/yx/appgameservice/controller/EchoController.java
@@ -1,7 +1,6 @@
 package com.yx.appgameservice.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,11 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date 2019/10/24 15:02
  **/
 @RestController
-@RefreshScope
 public class EchoController {
 
 
-    @Value("${myName}")
+    @Value("${uuu}")
     private String str;
 
     @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
