4bde8fe9c4bd0958cc7da9fd4de8b998de08fa4b
Jesse <Jesse@seektopser.com>
2019-10-28 10:35:07 +0800

    开启feign服务降级

--- app-web-service/src/main/java/com/yx/appwebservice/AppWebServiceApplication.java
+++ app-web-service/src/main/java/com/yx/appwebservice/AppWebServiceApplication.java
@@ -1,17 +1,9 @@
 package com.yx.appwebservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
