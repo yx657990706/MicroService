75a9d40d7fdf7654ba44dff7ee6036f5b6d79c79
Jesse <Jesse@seektopser.com>
2020-01-26 17:46:09 +0800

    提供一种API跳过token验证处理的方式

--- base-gateway-sentinel/src/main/java/com/yx/basegatewaysentinel/filter/AuthorizeTokenFilter.java
+++ base-gateway-sentinel/src/main/java/com/yx/basegatewaysentinel/filter/AuthorizeTokenFilter.java
@@ -9,8 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 /**
  * token全局过滤器
@@ -20,18 +24,29 @@ import reactor.core.publisher.Mono;
 public class AuthorizeTokenFilter implements GlobalFilter {
 
     private static final String AUTHORIZE_TOKEN = "token";
+    /**
+     * 排除签名验证的API
+     */
+    private static List<String> SIGN_EXCLUDE;
+    /**
+     * 路径匹配验证器
+     */
+    private static PathMatcher PATH_MATCHER = new AntPathMatcher();
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpResponse response = exchange.getResponse();
-
-        //TODO 部分URL放行处理（取配置文件，假设规则为某种路径开头的放行）
-        boolean skip = false;
+        ServerHttpRequest request = exchange.getRequest();
+        // 获取请求的路径
+        String requestPath = request.getURI().getRawPath();
+        //TODO 部分URL放行处理（取配置文件，获取放行路径list，假设规则为某种路径开头的放行）
+//        SIGN_EXCLUDE = null;//放行API
+        boolean skip = this.checkSkip(requestPath);
         if(skip){
             return chain.filter(exchange);
         }
         //获取请求头中的token信息
-        ServerHttpRequest request = exchange.getRequest();
+
         HttpHeaders headers = request.getHeaders();
         String token = headers.getFirst(AUTHORIZE_TOKEN);
         //token校验
@@ -50,5 +65,17 @@ public class AuthorizeTokenFilter implements GlobalFilter {
         }
 
         return chain.filter(exchange);
+    }
+
+    private boolean checkSkip(final String requestPath) {
+        if (SIGN_EXCLUDE.contains(requestPath)) {
+            return true;
+        }
+        for (String excludePath : SIGN_EXCLUDE) {
+            if (PATH_MATCHER.match(excludePath, requestPath)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
